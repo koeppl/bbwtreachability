ec7944aab34bdef9470c6debd4e4486dd307f6c9
Dominik Köppl <git@dkppl.de>
2024-06-11 14:23:36 +0900

    add flag -i for inverse for ./computepath.py

--- computepath.py
+++ computepath.py
@@ -5,7 +5,7 @@
 import argparse
 import bwt
 
-def path(start : str, end: str) -> str:
+def path(start : str, end: str, use_inverse : bool) -> str:
 	""" return the path as a string, where b is for BBWT, i for inverse BBWT, and the number x for the x-th left rotation """
 	paths = {start : [] }
 	stack = [start]
@@ -23,7 +23,8 @@ def path(start : str, end: str) -> str:
 		for i in reversed(range(len(curnode))):
 		#for i in range(len(curnode)):
 			update_stack(curnode[i:] + curnode[:i], str(i))
-		# update_stack(bwt.bijective_bwt_inv(curnode), 'i')
+		if use_inverse:
+			update_stack(bwt.bijective_bwt_inv(curnode), 'i')
 		update_stack(bwt.bijective_bwt(curnode), 'b')
 		stack.sort(key = lambda x : len(paths[x]), reverse=True)
 		# print(list(map(lambda x : len(paths[x]), stack)))
@@ -34,6 +35,7 @@ def path(start : str, end: str) -> str:
 parser = argparse.ArgumentParser(description='outputs the path as a string, where b is for BBWT, i for inverse BBWT, and the number x for the x-th left rotation')
 parser.add_argument('--start', '-s', required=True, type=str, nargs='?', help='string from we start', default=2)
 parser.add_argument('--target', '-t', required=True, type=str, help='string where we want to go', default=3)
+parser.add_argument('--use_inverse', '-i', action='store_true', required=False, help='do you also want to consider the inverse?', default=False)
 args = parser.parse_args()
 
-print(path(args.start, args.target))
+print(path(args.start, args.target, args.use_inverse))
